d7b8bada6356ec1405025f2c0484dd9ac85a1f9c
Crowdbotics <team@crowdbotics.com>
2021-01-22 22:59:07 +0000

    Update models

--- backend/the_success_collect_23999/wsgi.py
+++ backend/the_success_collect_23999/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'the_success_collect_23999.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "the_success_collect_23999.settings")
 
 application = get_wsgi_application()
